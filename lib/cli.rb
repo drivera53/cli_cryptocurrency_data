3adc8dd95b9ff06bc533030f728c3baadc1fe322
Danny Rivera <samboard@hotmail.com>
2021-02-25 09:11:38 -0500

    Editing get_full_data method in cryptocurrency

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,19 +1,50 @@
 class FINDCRYPTO::CLI
     def initialize
-        FINDCRYPTO::API.new.get_top_20_cryptocurrencies
+        FINDCRYPTO::API.new.get_top_20_cryptocurrencies 
+        puts "Crypto Is Everywhere, But Should You Invest?"
     end
 
     def run
         greeting
+        enter_key = gets.strip
+        if enter_key == ''
+            menu
+        end
+        #menu
     end
 
     def greeting
-        puts "This is the greeting method"
-        puts "CLI FILE CONNECTED"
-        puts "Grabbing Data.... Loading...."
+        puts "Some experts have suggested that Bitcoin could be worth as much as $100,000 one day!"
+        puts "But, not everyone thinks investing in cryptocurrency is a good idea."
+        puts "Press enter to see valuable data and make your own conclusions:"
     end
 
     def menu
-        puts "Pick something from menu"
+        # until input == "exit"
+        puts "Top 20 Cryptocurrency Prices by Market Cap:"
+        display_top_20_cryptocurrencies
+        input = gets
+        FINDCRYPTO::Cryptocurrency.all[input.to_i - 1].get_full_data
+        puts "Would you like to see data of another cryptocurrency? Enter Yes or No"
+        back_to_menu_input = gets.strip.downcase
+        if back_to_menu_input == "yes"
+            menu
+        #elsif back_to_menu_input == "no"
+            #puts "If you don’t believe it or don’t get it, I don’t have the time to try to convince you, sorry."
+            #puts "– Satoshi Nakamoto"
+        else
+            puts "If you don’t believe it or don’t get it, I don’t have the time to try to convince you, sorry."
+            puts "– Satoshi Nakamoto"
+            exit
+        end    
     end
+
+    def display_top_20_cryptocurrencies
+        puts "#  COIN:                   PRICE:"
+        FINDCRYPTO::Cryptocurrency.all.each.with_index(1) do |cryptocurrency, index|
+            puts "#{index}. #{cryptocurrency.name}              Price: $#{cryptocurrency.current_price}"
+        end
+        puts "What crypto would you like more information on? (e.g. 1 for Bitcoin)"
+    end
+
 end
--- lib/cryptocurrency.rb
+++ lib/cryptocurrency.rb
@@ -13,8 +13,24 @@ class FINDCRYPTO::Cryptocurrency
         @@all
     end
 
-    def get_name
-        self.name
+    def get_full_data
+        puts "--------------------$ #{self.name.upcase} $--------------------"
+        puts "#{self.name} can be purchased on peer-to-peer networks"
+        puts "and cryptocurrency exchanges, just like buying stocks"
+        puts "-----------------------$ DATA $------------------------"
+        puts "*** This data is valid as #{convert_to_local_time(self.last_updated)} ***"
+        puts "Price as date above: $#{self.current_price}"
+        puts "24 Hour High: $#{self.high_24h}       24 Hour Low: $#{self.low_24h}"
+        puts "Market Cap: $#{self.market_cap}       Market Cap Rank: ##{self.market_cap_rank}"
+
+    end
+
+    private
+    def convert_to_local_time(utc_time) # convert_to_local_time(hash[0]["last_updated"])
+        utc_parsed = Time.parse(utc_time)
+        utc_parsed.localtime
     end
 
 end
+
+#convert_to_local_time(self.last_updated)
